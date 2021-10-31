7ea99db0690a366d00fcaaad897ff93c855e7f74
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-10-31 09:31:30 -0500

    added routes to html files

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+// required modules
 const express = require('express');
-const { notes } = require('./db/db');
+const fs = require('fs');
+const path = require('path');
 
+// port declaration
 const PORT = process.env.PORT || 3003;
+
+// initiate the server
 const app = express();
 
+app.use(express.urlencoded ( { extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/api/db', (req, res) => {
+// request database
+const { notes } = require ('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
   res.send('Hello!');
-})
+});
+
+// route to index.html 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
+
+// route to notes.html 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/notes.html'));
+}); 
 
 app.listen(PORT, () => {
-    console.log(`API server now live at http://localhost:${PORT} !`);
-  });
+    console.log(`API server now live at http://localhost:${PORT}!`);
+});
+
